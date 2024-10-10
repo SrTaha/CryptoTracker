9c3beb014418cfb198a89e430ca4e8af1d7af5dc
Taha Majlesi Pour <srtaha2022@gmail.com>
2024-10-09 20:40:35 -0700

    update

--- js/CryptoLoader.js
+++ js/CryptoLoader.js
@@ -30,12 +30,34 @@ function LoadData() {
     .then((response) => response.json())
     .then((responseData) => {
       responseData.data.forEach((elem) => {
-        let div = document.createElement("div");
-        div.classList.add("w-4/12");
-        let obj = document.createElement("canvas");
-        obj.id = elem.id;
-        div.appendChild(obj);
-        root.appendChild(div);
+        //-----------------------------------------------
+        let ChartContainers = document.createElement("ChartContainers");
+        ChartContainers.classList.add(
+          "w-8/12",
+          "mx-auto",
+          "p-4",
+          "bg-red-500",
+          "my-5"
+        );
+        //-----------------------------------------------
+        let ChartHeader = document.createElement("p");
+        ChartHeader.innerHTML = `${elem.rank} - ${elem.name} (${elem.symbol})`;
+        ChartHeader.classList.add("font-black");
+        ChartContainers.appendChild(ChartHeader);
+        //-----------------------------------------------
+        let objChart = document.createElement("canvas");
+        objChart.id = elem.id;
+        ChartContainers.appendChild(objChart);
+        //-----------------------------------------------
+        let ChartInfo = document.createElement("p");
+        ChartInfo.innerHTML = `Current Price: ${elem.price_usd}$ <br> Price Per BTC : ${elem.price_btc} <br> Total MarketCap : ${elem.market_cap_usd}$ <br> Last Price per hour : ${elem.percent_change_1h}$
+        <br> Last Price per 24h : ${elem.percent_change_24h}$
+        <br> Last Price per week : ${elem.percent_change_7d}$
+        <br> Trades per 24h : ${elem.volume24}`;
+        ChartInfo.classList.add("font-black", "m-3");
+        objChart.after(ChartInfo);
+        //-----------------------------------------------
+        root.appendChild(ChartContainers);
         ChartGenerator(document.getElementById(`${elem.id}`));
       });
     });
