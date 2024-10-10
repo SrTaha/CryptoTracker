79360b7cf7709017e895bd2b5210337439d935b7
Taha Majlesi Pour <srtaha2022@gmail.com>
2024-10-10 07:08:56 -0700

    Chart.js Removed , layout changed

--- js/CryptoLoader.js
+++ js/CryptoLoader.js
@@ -1,35 +1,39 @@
 let root = document.getElementById("root");
-
+let nav = document.getElementById("nav");
 let html = ``;
 
-root.innerHTML = `<nav class="bg-gray-800 p-3 relative top-0 flex items-center">
+nav.innerHTML = `<nav class="bg-gray-800 p-3 relative top-0 flex items-center">
         <a href="https://github.com/SrTaha/CryptoTracker" target="_blank" class="font-black text-white mx-auto">Crypto
             Tracker : Track Coins Price At Any Time </a>
     </nav>`;
 
 //-----------------------------------------------
-function ChartGenerator(ctx) {
-  new Chart(ctx, {
-    type: "line",
-    data: {
-      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-      datasets: [
-        {
-          label: "# of Votes",
-          data: [12, 19, 3, 5, 2, 3],
-          borderWidth: 1,
-        },
-      ],
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true,
-        },
-      },
-    },
-  });
-}
+// function ChartGenerator(ctx) {
+//   new Chart(ctx, {
+//     type: "line",
+//     data: {
+//       labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+//       datasets: [
+//         {
+//           label: "# of Votes",
+//           data: [12, 19, 3, 5, 2, 3],
+//           borderWidth: 1,
+//         },
+//       ],
+//     },
+//     options: {
+//       scales: {
+//         y: {
+//           beginAtZero: true,
+//         },
+//       },
+//     },
+//   });
+// }
+
+// let objChart = document.createElement("canvas");
+// objChart.id = elem.id;
+// ChartContainers.appendChild(objChart);
 
 function LoadData() {
   fetch("https://api.coinlore.net/api/tickers/")
@@ -40,7 +44,8 @@ function LoadData() {
         let ChartContainers = document.createElement("ChartContainers");
         ChartContainers.classList.add(
           "w-11/12",
-          "lg:w-8/12",
+          "md:w-5/12",
+          "lg:w-2/12",
           "mx-auto",
           "p-4",
           "bg-gray-600",
@@ -53,20 +58,15 @@ function LoadData() {
         ChartHeader.classList.add("font-black");
         ChartContainers.appendChild(ChartHeader);
         //-----------------------------------------------
-        let objChart = document.createElement("canvas");
-        objChart.id = elem.id;
-        ChartContainers.appendChild(objChart);
-        //-----------------------------------------------
         let ChartInfo = document.createElement("p");
         ChartInfo.innerHTML = `Current Price: ${elem.price_usd}$ <br> Price Per BTC : ${elem.price_btc} <br> Total MarketCap : ${elem.market_cap_usd}$ <br> Last Change per hour : ${elem.percent_change_1h}$
         <br> Last Change per 24h : ${elem.percent_change_24h}$
         <br> Last Change per week : ${elem.percent_change_7d}$
         <br> Trades per 24h : ${elem.volume24}`;
         ChartInfo.classList.add("font-black", "m-3");
-        objChart.after(ChartInfo);
+        ChartHeader.after(ChartInfo);
         //-----------------------------------------------
         root.appendChild(ChartContainers);
-        ChartGenerator(document.getElementById(`${elem.id}`));
       });
     });
 }
