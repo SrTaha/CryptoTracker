9a1c82667d883416c6c44b99318b7d6e5f713bad
Taha Majlesi Pour <srtaha2022@gmail.com>
2024-10-17 08:21:35 -0700

    nothing

--- js/AddData.js
+++
@@ -1,9 +0,0 @@
-let Item = "";
-
-function CardMaker(nameId, rank, name, symbol) {
-  $(Item).html(`
-        <div id=${nameId}>
-        <p>${rank} - ${name} (${symbol})</p>
-        </div>`);
-  $(Item).appendTo();
-}
--- js/Assets.js
+++
@@ -1,3 +0,0 @@
-console.log(
-  "Api That I Used For Crypto Data : https://api.coinlore.net/api/tickers/"
-);
